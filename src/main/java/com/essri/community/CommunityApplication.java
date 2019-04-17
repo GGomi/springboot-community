42c51b7713659b6f930ea7e0053ef101b0b39af5
ggomi <dhkdn4042@naver.com>
2019-04-18 02:27:29 +0900

    Insert Default Data

--- src/main/java/com/essri/community/CommunityApplication.java
+++ src/main/java/com/essri/community/CommunityApplication.java
@@ -1,7 +1,16 @@
 package com.essri.community;
 
+import com.essri.community.domain.Board;
+import com.essri.community.domain.BoardType;
+import com.essri.community.domain.User;
+import com.essri.community.repository.BoardRepository;
+import com.essri.community.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class CommunityApplication {
@@ -10,4 +19,25 @@ public class CommunityApplication {
         SpringApplication.run(CommunityApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
+        return (args) -> {
+            User user = userRepository.save(User.builder()
+                    .name("essri")
+                    .email("essri@gmail.com")
+                    .password("test")
+                    .build()
+            );
+
+            IntStream.rangeClosed(1, 200).forEach(index ->
+                    boardRepository.save(Board.builder()
+                            .user(user)
+                            .title("제목 / " + index)
+                            .subTitle("부제목 / " + index)
+                            .content("#" + index)
+                            .boardType(BoardType.free)
+                            .build())
+            );
+        };
+    }
 }
