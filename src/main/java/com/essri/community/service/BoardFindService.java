ed9c4ecf16bcbf3852f2dcbc69ea697bd6c02f89
ggomi <dhkdn4042@naver.com>
2019-04-18 02:27:18 +0900

    List Order By DESC

--- src/main/java/com/essri/community/service/BoardFindService.java
+++ src/main/java/com/essri/community/service/BoardFindService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,11 +16,15 @@ public class BoardFindService {
     private final BoardRepository boardRepository;
 
     public Page<Board> findBoardList(Pageable pageable) {
-        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
+
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, new Sort(Sort.Direction.DESC, "id"));
+
         return boardRepository.findAll(pageable);
+
     }
 
-    public Board findBoardById(Long idx) {
-        return boardRepository.findById(idx).orElse(Board.builder().build());
+    public Board findBoardById(Long id) {
+        return boardRepository.findById(id).orElse(Board.builder().build());
     }
 }
